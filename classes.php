d4d533d71586d507da02104333f983788beaed24
Gabriel.SK <gabrielkonrad.rs@gmail.com>
2026-09-10 17:42:28 -0300

    atividade feita só falta os animais que esta no pc da escola

--- classes.php
+++ classes.php
@@ -95,4 +95,121 @@ class Plantas {
 class Animais {
     public string $nome;
 }
+
+class Elemento {
+    public string $nome;
+    public string $simbolo;
+    public int $numeroAtomico;
+    public float $massaAtomica;
+    public string $grupo;
+    public string $periodo;
+    public string $estadoFisico;
+    public int $quantidade;
+
+    public function __construct(string $nome, string $simbolo, int $numeroAtomico, float $massaAtomica, string $grupo, string $periodo, string $estadoFisico, int $quantidade)
+    {
+        $this->nome = $nome;
+        $this->simbolo = $simbolo;
+        $this->numeroAtomico = $numeroAtomico;
+        $this->massaAtomica = $massaAtomica;
+        $this->grupo = $grupo;
+        $this->periodo = $periodo;
+        $this->estadoFisico = $estadoFisico;
+        $this->quantidade = $quantidade;
+    }
+
+    public function aumentarQuantidade($maisquantidade){
+        $this->quantidade = $this->quantidade + $maisquantidade;    
+        echo "Quantidade aumentada para o exercicio para $this->quantidade.<br>";
+    }
+
+    public function menosQuantidade($menosquantidade){
+        $this->quantidade = $this->quantidade + $menosquantidade;    
+        echo "Quantidade aumentada do elemento $this->nome para o exercicio para $this->quantidade.<br>";
+    }
+
+    public function mostrarElemento() {
+        echo "<ul?>";
+            echo "<li>$this->nome</li>";
+            echo "<li>$this->simbolo</li>";
+            echo "<li>$this->numeroAtomico</li>";
+            echo "<li>$this->massaAtomica</li>";
+            echo "<li>$this->grupo</li>";
+            echo "<li>$this->periodo</li>";
+            echo "<li>$this->estadoFisico</li>";
+        echo "</ul> <br>";
+    }
+}
+
+class Bicicleta {
+    public string $marca;
+    public string $modelo;
+    public string $cor;
+    public float $velocidade;
+    public int $marchas;
+    public int $marchaAtual;
+    public int $roda;
+
+    public function __construct(string $marca, string $modelo, string $cor, float $velocidade, int $marchas, int $marchaAtual, int $roda)
+    {
+        $this->marca = $marca;
+        $this->modelo = $modelo;
+        $this->cor = $cor;
+        $this->velocidade = $velocidade;
+        $this->marchas = $marchas;
+        $this->marchaAtual = $marchaAtual;
+        $this->roda = $roda;
+    }
+
+    public function acelerar($aumento) {
+        $this->velocidade = $this->velocidade + $aumento;
+        echo "Você aumento a velocidade para $this->velocidade km/h<br>";
+    }
+
+    public function frear($diminuir){
+        $this->velocidade = $this->velocidade - $diminuir;
+        echo "Você diminuiu a velocidade para $this->velocidade km/h<br>";
+    }
+
+    public function Trocardemarcha($marcha){
+        $this->marchaAtual = $marcha;
+        echo "Marcha tocada para $this->marchaAtual";
+    }
+}
+
+class Planeta {
+    public string $nome;
+    public string $tipo;
+    public float $temperatura;
+    public float $gravidade;
+    public int $luas;
+    public float $distanciaSol;
+    public float $velocidade;
+
+    public function __construct(string $nome, string $tipo, float $temperatura, float $gravidade, int $luas, float $distanciaSol, float $velocidade)
+    {
+        $this->nome = $nome;
+        $this->tipo = $tipo;
+        $this->temperatura = $temperatura;
+        $this->gravidade = $gravidade;
+        $this->luas = $luas;
+        $this->distanciaSol = $distanciaSol;
+        $this->velocidade = $velocidade;
+    }
+
+    public function AumentarTemperatura($graus){
+        $this->temperatura = $this->temperatura + $graus;
+        echo "Temperatura do planeta foi aumentada para $this->temperatura<br>";
+    }
+
+    public function DiminuirTemperatura($graus){
+        $this->temperatura = $this->temperatura - $graus;
+        echo "Temperatura do planeta foi aumentada para $this->temperatura<br>";
+    }
+
+    public function alterarVelocidade($velocidade) {
+        $this->velocidade = $velocidade;
+        echo "A velocidade do planeta foi alterada para $this->velocidade<br>";
+    }
+}
 ?>
--- index.php
+++ index.php
@@ -6,4 +6,20 @@ $planta1->Cuidados(120, 500);
 $planta1->checagem();
 $planta1->crescer();
 $planta1->CrescerFolhas();
+
+
+$Elemento1 = new Elemento("Hélio", "He", 2, 4.0026, 18, 1, "Gasoso", 1);
+$Elemento1->menosQuantidade(1);
+$Elemento1->aumentarQuantidade(5);
+$Elemento1->mostrarElemento();
+
+$Bicicleta1 = new Bicicleta("Caloi", "Two Niner", "Preta", 0.0, 21, 1, 29);
+$Bicicleta1->acelerar(50);
+$Bicicleta1->frear(20);
+$Bicicleta1->Trocardemarcha(5);
+
+$Planeta1 = new Planeta("Marte", "Rochoso", -63.0, 3.71, 2, 227.9, 24.07);
+$Planeta1->AumentarTemperatura(40);
+$Planeta1->DiminuirTemperatura(15);
+$Planeta1->AumentarTemperatura(60);
 ?>
